9716535693e7680eac4d8bcb73030489dfd55e0c
protasovse <protasovse@gmail.com>
2018-12-31 11:21:24 +0500

    min memory

--- F.py
+++ F.py
@@ -1,23 +1,11 @@
 import sys
 
-int_list, counter, pointer = [], [], []
+int_list = []
 n = int(sys.stdin.readline().strip())
 for i in range(int(n)):
-    s = input()
+    s = sys.stdin.readline().strip()
     li = s.split(' ')
     num = int(li[0])
-    counter.append(num+1)
-    pointer.append(1)
-    int_list.append(list([int(l) for l in li[0:num + 1]]))
-
-while counter != pointer:
-    mi, j, i = 100, 0, 0
-    for l in int_list:
-        if pointer[i] < counter[i]:
-            if l[pointer[i]] <= mi:
-                mi = l[pointer[i]]
-                j = i
-        i += 1
-    pointer[j] += 1
-    sys.stdout.write('%d ' % (mi, ))
-print('\n')
+    int_list.extend([int(l) for l in li[1:num + 1]])
+for i in sorted(int_list):
+    sys.stdout.write('%d ' % (i,))

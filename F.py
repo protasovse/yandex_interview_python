6a333a50c0cbd0e9628740cd2241f520f5ebffdd
protasovse <protasovse@gmail.com>
2019-01-02 09:51:57 +0500

    all string

--- F.py
+++ F.py
@@ -1,11 +1,25 @@
 import sys
 
-int_list = []
+int_str = ''
 n = int(sys.stdin.readline().strip())
 for i in range(int(n)):
-    s = sys.stdin.readline().strip()
-    li = s.split(' ')
-    num = int(li[0])
-    int_list.extend([int(l) for l in li[1:num + 1]])
-for i in sorted(int_list):
-    sys.stdout.write('%d ' % (i,))
+    s = sys.stdin.readline().strip() + ' '
+    count = int(s[:s.find(' ')])
+    p, j = 0, 0
+    for j in range(s.__len__()):
+        if s[j] == ' ':
+            p += 1
+        if p == count+1:
+            break
+    int_str += s[s.find(' '):j]
+    del (s,)
+
+for i in sorted(int_str.lstrip().split(' '), key=lambda x: int(x) if x.isdigit() else 0):
+    print(i, end=" ")
+
+# 5
+# 5 2 26 64 88 96 96
+# 4 8 20 65 86
+# 7 1 4 16 42 58 61 69
+# 1 84
+# 0

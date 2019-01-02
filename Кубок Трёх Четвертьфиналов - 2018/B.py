90bb3f6411717cac9a6ae97d439265950aa9b62e
protasovse <protasovse@gmail.com>
2019-01-02 17:29:30 +0500

    B. A+B 2

--- Кубок Трёх Четвертьфиналов - 2018/B.py
+++ Кубок Трёх Четвертьфиналов - 2018/B.py
@@ -1,5 +1,5 @@
-file = open('./data.txt', 'r').readline()
+file = open('./input.txt', 'r').readline()
 n = [int(x) for x in file.strip().split(' ')]
-file2 = open('./res.txt', 'w')
+file2 = open('./output.txt', 'w')
 file2.write(f'{n[0]+n[1]}')
 file2.close()
